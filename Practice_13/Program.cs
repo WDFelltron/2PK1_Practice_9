8ec0b3b3b90bd52b4c5e53db019c6083de8e23c6
k.lapuzin@outlook.com <k.lapuzin@outlook.com>
2022-11-14 10:48:37 +0500

    сделал 13 практику

---
+++ Practice_13/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+namespace Practice_13
+{
+    public class Program
+    {
+        public static bool IsPalindrom(string sent)
+        {
+            sent.Replace(" ", "");
+            bool right = false;
+            if(sent.Length%2==0)
+            {
+                for(int i = 0; i < sent.Length / 2; i++)
+                {
+                    if (sent[i] == sent[sent.Length - 1])
+                    {
+                        right = true;
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+            }
+            else
+            {
+                for (int i = 0; i < (sent.Length - 1) / 2; i++)
+                {
+                    if (sent[i] == sent[sent.Length - 1])
+                    {
+                        right = true;
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+            }
+            return right;
+        }
+        public static void Main(string[] args)
+        {
+            Console.WriteLine(IsPalindrom(Console.ReadLine()));
+        }
+    }
+}
